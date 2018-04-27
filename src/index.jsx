6b0ed2c7c918c26217ebd774f04a3c07a2d228ce
Michael Harrison <harrim91@hotmail.co.uk>
2018-04-27 21:02:58 +0100

    Remove component file, render plain element

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,5 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app';
 
-render(
-  <App />,
-  document.getElementById('root'),
-);
+render(<h1>Hello World!</h1>, document.getElementById('root'));
