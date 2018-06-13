c8f97f0563168af1948d05b3ec299f4694fbad8c
Joe Stephens <joe@webantic.co.uk>
2018-06-13 16:27:19 +0100

    Get repo ready for projects

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,6 @@ import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<h1>Hello World!</h1>, document.getElementById('root'));
+render((
+  <h1>Hello World!</h1>
+), document.getElementById('root'));
