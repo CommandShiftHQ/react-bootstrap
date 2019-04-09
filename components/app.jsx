41e15177fe59af3d0c19e1d27ca7eee3d69ce829
tbilbe <tbilbe@gmail.com>
2019-04-09 21:10:03 +0100

    component folder created, dummy component added w/ prop

---
+++ components/app.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = (props) => <h1>Hello from inside the h1. {props.name} created this he is {props.age} old.</h1>;
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import App from '../components/app';
 
-render(<h1>Hello World!</h1>, document.getElementById('root'));
+
+render(<App name="Thomas" age="30" />, document.getElementById('root'));
