a2096d640169aa7a0bc1c095e1a32e0330628f6a
Susana Rodriguez <susana.rodriguez@bbc.co.uk>
2019-04-10 19:11:32 +0100

    Initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import 'raf/polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/app';
 
-render(<h1>Hello World!</h1>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
