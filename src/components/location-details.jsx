4ebd66a3836e31378021af2d8c47e62e4e61900a
Nesu11 <anesujane98@gmail.com>
2019-04-14 14:07:21 +0100

    location

--- src/components/location-details.jsx
+++ src/components/location-details.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const LocationDetails = props => (
-  <h1 className="location-details"> {props.city}, {props.country} </h1>
+  <h1 className="location-details">{props.city}, {props.country}</h1>
 );
 LocationDetails.propTypes = {
   city: PropTypes.string.isRequired,
