832a486ca4f7245d7f486acf36701f03b62412aa
Nesu11 <anesujane98@gmail.com>
2019-04-14 14:07:56 +0100

    forecast update

---
+++ src/components/forecast-summary.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ForecastSummary = props => (
+  <div>
+    <div className="forecast-summary__date"><span>{props.date}</span></div>
+
+    <div className="forecast-summary__temperature"><span>{props.temperature}</span></div>
+
+    <div className="forecast-summary__description"><span>{props.description}</span></div>
+
+    <div className="forecast-summary__icon"><span>{props.icon}</span></div>
+  </div>
+)
+
+
+
+export default ForecastSummary;
