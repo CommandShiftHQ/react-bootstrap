a1a70c8e626ab0b106c3a1f39c10bcc7d00cf805
Nesu11 <anesujane98@gmail.com>
2019-04-14 13:42:41 +0100

    forecast summary tests

---
+++ __tests__/components/forecast-summary.test.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ForecastSummary from '../../src/components/forecast-summary';
+
+it('renders the date, description, icon and max temp for the day', () => {
+  const wrapper = shallow((
+    <ForecastSummary
+      date="mockDate"
+      temperature="mockTemperature"
+      description="mockDescription"
+      icon="mockIcon"
+    />
+
+  ));
+  expect(wrapper.find('.forecast-summary__date').text()).toEqual('mockDate');
+});
+
+it('renders the temperature for the day', () => {
+  const wrapper = shallow((
+    <ForecastSummary
+      date="mockDate"
+      temperature="mockTemperature"
+      description="mockDescription"
+      icon="mockIcon"
+    />
+  ));
+  expect(wrapper.find('.forecast-summary__temperature').text()).toEqual('mockTemperature');
+});
+
+it('renders a description', () => {
+  const wrapper = shallow((
+    <ForecastSummary
+      date="mockDate"
+      temperature="mockTemperature"
+      description="mockDescription"
+      icon="mockIcon"
+    />
+  ));
+  expect(wrapper.find('.forecast-summary__description').text).toEqual('mockDescription');
+});
+
+it('renders an icon', () => {
+  const wrapper = shallow((
+    <ForecastSummary
+      date="mockDate"
+      temperature="mockTemperature"
+      description="mockDescription"
+      icon="mockIcon"
+    />
+  ));
+  expect(wrapper.find('.forecast-summary__icon').text()).toEqual('mockIcon');
+});
