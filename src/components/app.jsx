d2e5566610ecf853180c6b3502cd79b0b7df9550
Cai Jenkins <Caisjenkins@gmail.com>
2019-08-12 10:43:08 +0100

    added test to the file

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import LocationDetails from './location-details';
 
-const App = () => <LocationDetails />;
+
+const App = props => (
+  <LocationDetails
+    city={props.location.city}
+    country={props.location.country}
+  />
+);
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
 
 export default App;
