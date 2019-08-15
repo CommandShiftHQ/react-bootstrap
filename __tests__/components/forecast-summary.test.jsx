246d5950586ef5754c4ee1da176e895cf580c0b1
Cai Jenkins <Caisjenkins@gmail.com>
2019-08-15 15:26:49 +0100

    added icons and date

--- __tests__/components/forecast-summary.test.jsx
+++ __tests__/components/forecast-summary.test.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-
 import { shallow } from 'enzyme';
-
 import ForecastSummary from '../../src/components/forecast-summary';
 
 it('renders the date', () => {
   const wrapper = shallow((
-
     <ForecastSummary
-
       date="mockDate"
-
       temperature="mockTemperature"
-
       description="mockDescription"
-
       icon="mockIcon"
-
     />
 
   ));
@@ -26,39 +18,23 @@ it('renders the date', () => {
 
 it('renders the temperature', () => {
   const wrapper = shallow((
-
     <ForecastSummary
-
       date="mockDate"
-
       temperature="mockTemperature"
-
       description="mockDescription"
-
       icon="mockIcon"
-
     />
-
   ));
-
   expect(wrapper.find('.forecast-summary__temperature').text()).toEqual('mockTemperature');
 });
-
 it('renders the description', () => {
   const wrapper = shallow((
-
     <ForecastSummary
-
       date="mockDate"
-
       temperature="mockTemperature"
-
       description="mockDescription"
-
       icon="mockIcon"
-
     />
-
   ));
 
   expect(wrapper.find('.forecast-summary__description').text()).toEqual('mockDescription');
--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WeatherIcon from 'react-icons-weather';
+import moment from 'moment';
 
 const ForecastSummary = props => (
   <h1>
-    <div className="forecast-summary__date">{props.date}</div>
+    <div className="forecast-summary__date">
+      <span>{moment(props.date).format('ddd Do MMM')}</span>
+    </div>
     <div className="forecast-summary__temperature">{props.temperature}</div>
     <div className="forecast-summary__description">{props.description}</div>
-    <div className="forecast-summary__icon">{props.icon}</div>
+    <div className="forecast-summary__icon">
+      <WeatherIcon name="owm" iconId={props.icon} />
+    </div>
   </h1>
 );
 
